7b43c36cd26b1061d0df853735f72c0f78819d0f
Francisco Gabriel <franciscogabrielf11@gmail.com>
2023-01-12 10:23:03 -0300

    refactor : Project  Pastes

--- lib/Routers/routes.dart
+++ lib/Routers/routes.dart
@@ -1,13 +1,15 @@
+// ignore_for_file: prefer_const_constructors
+
 import 'package:go_router/go_router.dart';
 
-import '../Views/HomePage/home_page.dart';
+import '../Views/HomePage/Screens/home_page.dart';
 
 final routes = GoRouter(
   initialLocation: '/HomePage',
   routes: [
     GoRoute(
       path: '/HomePage',
-      builder: (context, state) => const HomePage(),
+      builder: (context, state) => HomePage(),
     ),
   ],
 );
