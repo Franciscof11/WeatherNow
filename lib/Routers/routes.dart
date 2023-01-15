f297650ba37e7513b55937dc11c97bd2eed17da7
Francisco Gabriel <franciscogabrielf11@gmail.com>
2023-01-15 15:48:29 -0300

    feat : SplashScreen Config

--- lib/Routers/routes.dart
+++ lib/Routers/routes.dart
@@ -3,12 +3,18 @@
 import 'package:app/Views/HomePage/Screens/home_page.dart';
 import 'package:go_router/go_router.dart';
 
+import '../Views/SplashScreen/splash_screen.dart';
+
 final routes = GoRouter(
-  initialLocation: '/HomePage',
+  initialLocation: '/SplashScreen',
   routes: [
     GoRoute(
       path: '/HomePage',
       builder: (context, state) => HomePage(),
+    ),
+    GoRoute(
+      path: '/SplashScreen',
+      builder: (context, state) => SplashScreen(),
     ),
   ],
 );
