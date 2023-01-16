eaa1208040fd6882315cbc31d683b98b90c7ab92
Francisco Gabriel <franciscogabrielf11@gmail.com>
2023-01-16 19:13:15 -0300

    refactor : Splash Screen

--- lib/Views/SplashScreen/splash_screen.dart
+++ lib/Views/SplashScreen/splash_screen.dart
@@ -2,7 +2,6 @@
 
 import 'package:animated_splash_screen/animated_splash_screen.dart';
 import 'package:flutter/material.dart';
-import 'package:lottie/lottie.dart';
 import 'package:page_transition/page_transition.dart';
 
 import '../../Routers/routes.dart';
@@ -13,14 +12,12 @@ class SplashScreen extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return AnimatedSplashScreen(
-        splash: Lottie.network(
-            'https://assets3.lottiefiles.com/packages/lf20_h8iveK.json'),
+        splash: Image.asset('assets/Images/splash.gif'),
         pageTransitionType: PageTransitionType.fade,
         splashTransition: SplashTransition.fadeTransition,
-        animationDuration: Duration(seconds: 1),
-        duration: 4600,
-        backgroundColor: Color.fromRGBO(44, 46, 56, 1),
-        splashIconSize: 1000,
+        duration: 5500,
+        backgroundColor: Color.fromRGBO(228, 240, 246, 1),
+        splashIconSize: 80000,
         nextScreen: MaterialApp.router(
           debugShowCheckedModeBanner: false,
           routerConfig: routes,
