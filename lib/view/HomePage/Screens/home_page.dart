a6d5b7ec547f069e0e8c50af64d25b9f12ba762a
Francisco Gabriel <franciscogabrielf11@gmail.com>
2023-08-16 21:57:50 -0300

    feat: Push to refresh riverpod

--- lib/view/HomePage/Screens/home_page.dart
+++ lib/view/HomePage/Screens/home_page.dart
@@ -13,7 +13,7 @@ import '../Widgets/picker_cidade.dart';
 
 final weatherProvider = FutureProvider<Weather>((ref) async {
   final apiService = ref.watch(apiServiceProvider);
-  return apiService.getCurrentWeather('Sao Paulo');
+  return apiService.getCurrentWeather('Sao Luis');
 });
 
 class HomePage extends ConsumerWidget {
@@ -23,88 +23,99 @@ class HomePage extends ConsumerWidget {
     final weather = ref.watch(weatherProvider);
     return Scaffold(
       body: SafeArea(
-        child: Container(
-            height: MediaQuery.of(context).size.height,
-            width: MediaQuery.of(context).size.width,
-            decoration: BoxDecoration(
-                gradient: LinearGradient(
-              colors: [
-                Color.fromRGBO(41, 178, 221, 1),
-                Color.fromRGBO(51, 170, 221, 1),
-                Color.fromRGBO(45, 200, 234, 1),
-              ],
-              begin: Alignment.topCenter,
-              end: Alignment.bottomRight,
-            )),
-            child: RemoveGlowEffect(
-              child: SingleChildScrollView(
-                child: Column(
-                  children: [
-                    SizedBox(height: 20),
-                    Row(
-                      mainAxisAlignment: MainAxisAlignment.center,
-                      children: [
-                        PickerCidade(),
-                      ],
-                    ),
-                    SizedBox(height: 5),
-                    CurrentWeatherAnimation(weatherAnimation: 'cloud_and_sun'),
-                    SizedBox(height: 5),
-                    Padding(
-                      padding: const EdgeInsets.only(left: 8),
-                      child: GestureDetector(
-                        onTap: () {},
-                        child: weather.when(
-                          data: (data) {
-                            return GestureDetector(
-                              onTap: () async {
-                                final repo = WeatherRepository();
-                                final weather =
-                                    await repo.getCurrentWeather('Sao Paulo');
-                                print('----------------------------');
-                                print(weather.current?.tempC);
-                              },
-                              child: HomePageText(
-                                data: '22ºc',
-                                fontSize: 68,
-                                fontWeight: FontWeight.w600,
+        child: RefreshIndicator(
+          displacement: 85,
+          onRefresh: () => ref.refresh(weatherProvider.future),
+          child: weather.when(
+            data: (data) {
+              return Container(
+                  height: MediaQuery.of(context).size.height,
+                  width: MediaQuery.of(context).size.width,
+                  decoration: BoxDecoration(
+                      gradient: LinearGradient(
+                    colors: [
+                      Color.fromRGBO(41, 178, 221, 1),
+                      Color.fromRGBO(51, 170, 221, 1),
+                      Color.fromRGBO(45, 200, 234, 1),
+                    ],
+                    begin: Alignment.topCenter,
+                    end: Alignment.bottomRight,
+                  )),
+                  child: RemoveGlowEffect(
+                    child: SingleChildScrollView(
+                      physics: BouncingScrollPhysics(),
+                      child: Column(
+                        children: [
+                          SizedBox(height: 20),
+                          Row(
+                            mainAxisAlignment: MainAxisAlignment.center,
+                            children: [
+                              PickerCidade(),
+                            ],
+                          ),
+                          SizedBox(height: 5),
+                          CurrentWeatherAnimation(
+                              weatherAnimation: 'cloud_and_sun'),
+                          SizedBox(height: 5),
+                          Padding(
+                            padding: const EdgeInsets.only(left: 8),
+                            child: GestureDetector(
+                              onTap: () {},
+                              child: weather.when(
+                                data: (data) {
+                                  return HomePageText(
+                                    data: '${data.location!.localtime}ºc',
+                                    fontSize: 68,
+                                    fontWeight: FontWeight.w600,
+                                  );
+                                },
+                                error: (erro, _) {
+                                  return HomePageText(
+                                    data: 'erro',
+                                    fontSize: 68,
+                                    fontWeight: FontWeight.w600,
+                                  );
+                                },
+                                loading: () {
+                                  return CircularProgressIndicator();
+                                },
                               ),
-                            );
-                          },
-                          error: (erro, _) {
-                            return HomePageText(
-                              data: 'erro',
-                              fontSize: 68,
-                              fontWeight: FontWeight.w600,
-                            );
-                          },
-                          loading: () {
-                            return CircularProgressIndicator();
-                          },
-                        ),
+                            ),
+                          ),
+                          SizedBox(height: 5),
+                          HomePageText(
+                            data: weather.when(data: (data) {
+                              return 'Sensação térmica: ${data.current!.sensaTermica.toString()}ºc';
+                            }, error: (erro, _) {
+                              return 'a';
+                            }, loading: () {
+                              return '...';
+                            }),
+                            fontSize: 18,
+                            fontWeight: FontWeight.w400,
+                          ),
+                          SizedBox(height: 30),
+                          CurrentDayWidget(),
+                          SizedBox(height: 25),
+                          CurrentDayWidget(),
+                          SizedBox(height: 30),
+                        ],
                       ),
                     ),
-                    SizedBox(height: 5),
-                    HomePageText(
-                      data: weather.when(data: (data) {
-                        return 'a' /* {data.current.sensaTermica} */;
-                      }, error: (erro, _) {
-                        return 'a';
-                      }, loading: () {
-                        return 'Carregando...';
-                      }),
-                      fontSize: 18,
-                      fontWeight: FontWeight.w400,
-                    ),
-                    SizedBox(height: 30),
-                    CurrentDayWidget(),
-                    SizedBox(height: 25),
-                    CurrentDayWidget(),
-                    SizedBox(height: 30),
-                  ],
-                ),
-              ),
-            )),
+                  ));
+            },
+            error: (erro, _) {
+              return Center(
+                child: Text(erro.toString()),
+              );
+            },
+            loading: () {
+              return Center(
+                child: CircularProgressIndicator(),
+              );
+            },
+          ),
+        ),
       ),
     );
   }
